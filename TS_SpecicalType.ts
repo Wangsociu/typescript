a1dd6dba2144106980d4224233142d6a515289e5
Wangsociu <zietwangdihoc@gmail.com>
2024-04-04 14:51:11 +0700

    TS_Specialtypes

--- TS_SpecicalType.ts
+++ TS_SpecicalType.ts
@@ -1,11 +1,10 @@
-/*let u = true;
+let u = true;
 u = "string"; // error: type = 'string' not 'boolean
-math.round(u); // not number */
+math.round(u); // not number
 
 // any = any type ...
-/*let v: any = true;
+let v: any = true;
 v = 'string';
 console.log(Math.round(v)); // out = Nahh
-console.log(v);*/
-
+console.log(v);
 //never ffectively throws an error whenever it is defined.
