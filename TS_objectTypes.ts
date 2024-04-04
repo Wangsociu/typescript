4d3565719f7e671de6db7569dbae6557e8d0d958
Wangsociu <zietwangdihoc@gmail.com>
2024-04-04 14:59:47 +0700

    TS_objectTypes

---
+++ TS_objectTypes.ts
@@ -0,0 +1,18 @@
+const SV: {
+    ID: number,
+    CLASS: string,
+    Name: string
+} = {
+
+    ID: 61,
+    CLASS: 'D23CQCI01-N',
+    Name: 'Lung Thi Linh',
+};
+console.log(SV.ID);
+console.log(SV.CLASS);
+console.log(SV.Name);
+
+const Address: {[index: string]: number } = {};
+Address.HCM = 1927;
+console.log(Address.HCM);
+
