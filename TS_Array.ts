6097a46f153d21001603cad515305e4464eab4c2
Wangsociu <zietwangdihoc@gmail.com>
2024-04-04 14:43:07 +0700

    Ts_Array

---
+++ TS_Array.ts
@@ -0,0 +1,9 @@
+const names: string[] = [];
+names.push("Something","Nothing");
+console.log(names)
+
+// readonly -> k thay doi dc mang ..
+
+/*const Arrays: readonly string [] = ['Something'];
+Arrays.push("adudu");
+console.log(Arrays); --> error */
