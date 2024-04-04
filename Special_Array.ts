e76b43a73a0007ad425dec1cb8c504be87c56270
Wangsociu <zietwangdihoc@gmail.com>
2024-04-04 14:36:53 +0700

    nah

---
+++ Special_Array.ts
@@ -0,0 +1,11 @@
+/*let u = true;
+u = "string"; // error: type = 'string' not 'boolean
+math.round(u); // not number */
+
+// any = any type ...
+/*let v: any = true;
+v = 'string';
+console.log(Math.round(v)); // out = Nahh
+console.log(v);*/
+
+//never ffectively throws an error whenever it is defined.
