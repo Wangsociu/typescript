efd084bd3f0e810593901da6d9cf57eb4abd6641
Wangsociu <zietwangdihoc@gmail.com>
2024-04-04 14:49:09 +0700

    TS_Tuple

---
+++ TS_Tuple.ts
@@ -0,0 +1,10 @@
+// cho phep xac dinh type trong array
+let ourtuple: [number, boolean, string];
+
+ourtuple = [5, false, 'something was wrongs'];
+
+console.log(ourtuple);
+
+ourtuple.push('good');
+console.log(ourtuple);
+// n
