0bb2b7d5e544c9736f2b98d965e627d4078b89f3
Wangsociu <zietwangdihoc@gmail.com>
2024-04-06 10:48:15 +0700

    IF ELSE

---
+++ TS_ifelse.ts
@@ -0,0 +1,16 @@
+let a:number = 1;
+let b:number = 2;
+if (a>b) {
+    console.log('max = ',a);
+}
+else {
+    console.log('max = ',b);
+}
+
+let numb:number = 3;
+if (a%2==0) {
+    console.log('Day la so chan');
+}
+else if (a%2!=0) {
+    console.log('day la so le');
+}
