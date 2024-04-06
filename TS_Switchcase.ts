6ae44aee5c3307cf3e3bfb147a78af18700f76e6
Wangsociu <zietwangdihoc@gmail.com>
2024-04-06 10:56:43 +0700

    switchcase

---
+++ TS_Switchcase.ts
@@ -0,0 +1,12 @@
+let age:number = 99;
+
+switch (age) {
+    case 18:
+        console.log("Mới vào đời");
+        break;
+    case 20:
+        console.log("Trải một tí");
+        break;
+    default:
+        console.log(">>> run default");
+}
